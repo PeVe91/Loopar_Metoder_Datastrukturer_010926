9633e8061b6d254397046f08f379eeacb6a2156e
PeVe91 <per.vestin@iths.se>
2026-09-08 11:38:04 +0200

    Uppgift 4 + template uppgift 7

--- src/main/java/org/example/Uppg4.java
+++ src/main/java/org/example/Uppg4.java
@@ -4,6 +4,20 @@ package org.example;
  */
 public class Uppg4 {
     static void main() {
-        
+
+        String concatenated = "";
+        String inputString;
+        while(true) {
+
+            inputString = IO.readln("Enter a word: ");
+
+            if (inputString.isEmpty() || inputString.equals(".")) {
+                break;
+            }
+
+            concatenated += inputString;
+
+        }
+        IO.println(concatenated);
     }
 }
--- src/main/java/org/example/Uppg7.java
+++ src/main/java/org/example/Uppg7.java
@@ -4,6 +4,14 @@ package org.example;
  */
 public class Uppg7 {
     static void main() {
+        int[] numbers = {5, 12, 8, 21, 3};
 
+        int greatest = numbers[0];
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] > greatest){
+                greatest = numbers[i];
+            }
+        }
     }
 }
+
