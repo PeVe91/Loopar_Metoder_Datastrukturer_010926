a884de63d3d6aceeaa36506ce85aa262bfeda8bd
PeVe91 <per.vestin@iths.se>
2026-09-08 12:05:47 +0200

    Update Uppg5.java

--- src/main/java/org/example/Uppg5.java
+++ src/main/java/org/example/Uppg5.java
@@ -5,5 +5,26 @@ package org.example;
 public class Uppg5 {
     static void main() {
 
+        int count = 0;
+        int secretNumber = (int) (Math.random() * 100) + 1;
+
+        while (true) {
+            int inputNumber = Integer.parseInt(IO.readln("Take a guess: "));
+
+            if (inputNumber > secretNumber) {
+                count++;
+                IO.println("Too big.");
+            } else if (inputNumber < secretNumber) {
+                count++;
+                IO.println("Too small.");
+            } else {
+                count++;
+                break;
+            }
+        }
+
+        IO.println("You guessed the secret number: " + secretNumber);
+        IO.println("Number of tries: " + count);
+
     }
 }
