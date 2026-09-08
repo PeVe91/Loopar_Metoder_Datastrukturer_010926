1b3e1cdc1986ec3a6bb1e6d0aae70398924f4d9a
PeVe91 <per.vestin@iths.se>
2026-09-08 12:47:11 +0200

    Update Uppg6.java

--- src/main/java/org/example/Uppg6.java
+++ src/main/java/org/example/Uppg6.java
@@ -1,9 +1,53 @@
 package org.example;
 /*
+Programmet skriver ut:
 
- */
+#....
+.#...
+..#..
+...#.
+....#
+
+Varje gång y-loopen körs så körs *hela* x-loopen
+och därför skriver varje y-loop ut 5 tecken.
+
+När x-loopens inleds första gången är if-villkoret uppfyllt.
+Då skrivs "#" ut. I resterande fyra fall uppfylls else if-villkoret
+och varje loop skriver ut "." tills x-loopen är klar.
+
+Därefter uppfylls if-villkoret i x-loopen ett steg senare
+för varje gång y-loopen körs.
+
+
+*/
 public class Uppg6 {
     static void main() {
+        /*
+        Vad skriver följande program ut:
+        for(int y=0; y<5; y++) {
+   		    for(int x=0; x<5; x++) {
+       		    if( x == y )
+           		    System.out.print("#");
+       		    else
+           		    System.out.print(".");
+   		    }
+   		System.out.println("");
+        }
+
+        */
+
+        for(int y = 0; y <5 ; y++) {
+
+            for(int x = 0; x < 5; x++) {
+                if( x == y )
+                    System.out.print("#");
+                else
+                    System.out.print(".");
+            }
+
+            System.out.println("");
+
+        }
 
     }
 }
